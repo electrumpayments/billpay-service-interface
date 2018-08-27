6af0608854fbe83652b12077cedcd83a63ae1609
Kiuran <kiuran@protonmail.com>
2018-08-27 18:53:17 +0200

    CR changes

--- src/main/java/io/electrum/billpay/utils/Paths.java
+++ src/main/java/io/electrum/billpay/utils/Paths.java
@@ -6,23 +6,27 @@ public class Paths {
    public static final String BASE_PATH = "/billpay" + VERSION_NUMBER;
 
    // Account lookup paths
-   public static final String REQUEST_ACCOUNT_INFO = "/accountLookups/{" + PathParams.REQUEST_ID + "}";
-   public static final String REQUEST_TRAFFIC_FINE_INFO = "/accountLookups/traffic/{" + PathParams.REQUEST_ID + "}";
-   public static final String REQUEST_POLICY_INFO = "/accountLookups//policy/{" + PathParams.REQUEST_ID + "}";
+   public static final String ACCOUNT_LOOKUP_BASE_PATH = "/accountLookups";
+
+   public static final String REQUEST_ACCOUNT_INFO = ACCOUNT_LOOKUP_BASE_PATH + "/{" + PathParams.REQUEST_ID + "}";
+   public static final String REQUEST_TRAFFIC_FINE_INFO = ACCOUNT_LOOKUP_BASE_PATH + "/traffic/{" + PathParams.REQUEST_ID + "}";
+   public static final String REQUEST_POLICY_INFO = ACCOUNT_LOOKUP_BASE_PATH + "/policy/{" + PathParams.REQUEST_ID + "}";
 
    // Payment paths
-   public static final String CREATE_POLICY_PAYMENT = "/policy/{" + PathParams.PAYMENT_ID + "}";
-   public static final String CREATE_TRAFFIC_FINE_PAYMENT = "/traffic/{" + PathParams.PAYMENT_ID + "}";
-   public static final String CREATE_ACCOUNT_PAYMENT = "/{" + PathParams.PAYMENT_ID + "}";
-   public static final String CONFIRM_PAYMENT =
+   public static final String PAYMENTS_BASE_PATH = "/payments";
+
+   public static final String CREATE_POLICY_PAYMENT = PAYMENTS_BASE_PATH + "/policy/{" + PathParams.PAYMENT_ID + "}";
+   public static final String CREATE_TRAFFIC_FINE_PAYMENT = PAYMENTS_BASE_PATH +"/traffic/{" + PathParams.PAYMENT_ID + "}";
+   public static final String CREATE_ACCOUNT_PAYMENT = PAYMENTS_BASE_PATH +"/{" + PathParams.PAYMENT_ID + "}";
+   public static final String CONFIRM_PAYMENT = PAYMENTS_BASE_PATH +
          "/{" + PathParams.PAYMENT_ID + "}/confirmations/{" + PathParams.ADVICE_ID + "}";
-   public static final String REVERSE_PAYMENT =
+   public static final String REVERSE_PAYMENT = PAYMENTS_BASE_PATH +
          "/{" + PathParams.PAYMENT_ID + "}/reversals/{" + PathParams.ADVICE_ID + "}";
 
    // Refund paths
-   public static final String CREATE_REFUND = "/refunds/{" + PathParams.REFUND_ID + "}";
-   public static final String CONFIRM_REFUND =
-         "/refunds/{" + PathParams.REFUND_ID + "}/confirmations/{" + PathParams.ADVICE_ID + "}";
-   public static final String REVERSE_REFUND =
-         "/refunds/{" + PathParams.REFUND_ID + "}/reversals/{" + PathParams.ADVICE_ID + "}";
+   public static final String REFUNDS_BASE_PATH = "/refunds";
+
+   public static final String CREATE_REFUND = REFUNDS_BASE_PATH +"/{" + PathParams.REFUND_ID + "}";
+   public static final String CONFIRM_REFUND = REFUNDS_BASE_PATH + "/{" + PathParams.REFUND_ID + "}/confirmations/{" + PathParams.ADVICE_ID + "}";
+   public static final String REVERSE_REFUND = REFUNDS_BASE_PATH + "/{" + PathParams.REFUND_ID + "}/reversals/{" + PathParams.ADVICE_ID + "}";
 }
